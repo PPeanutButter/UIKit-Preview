82ff59fd093997e5b5b082978a1217034439c842
潘润秋 <panrunqiu@outlook.com>
2025-08-18 10:56:35 +0800

    添加部署脚本

--- composeApp/src/wasmJsMain/kotlin/com/peanut/uikit/preview/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/peanut/uikit/preview/main.kt
@@ -3,7 +3,6 @@ package com.peanut.uikit.preview
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
-import kotlinx.browser.window
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
